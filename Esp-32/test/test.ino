274ee3597f9013adffdd774e223ccf7cc2c61b8e
Willian Cárdenas <sca.willian@gmail.com>
2022-11-13 23:03:27 -0500

    fixed file structure, worse delay improved

--- Esp-32/test/test.ino
+++ Esp-32/test/test.ino
@@ -9,10 +9,118 @@ void setup()
   setup_bim();
   }
 
+void Sensor1code(void * pvParameters)
+{
+  for(;;)
+  {    
+    // * ULTRASONIC SENSOR 1 *
+    // Clears the trigPin
+    digitalWrite(trigPin_UP, LOW);
+    delayMicroseconds(2);
+    // Sets the trigPin on HIGH state for 10 micro seconds
+    digitalWrite(trigPin_UP, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin_UP, LOW);  
+  
+    // Reads the echoPin, returns the sound wave travel time in microseconds
+    duration_UP = pulseIn(echoPin_UP, HIGH); //ULTRASONIC SENSOR 1
+  
+    // Calculate the distance
+    distanceCm_UP = duration_UP * SOUND_SPEED/2; //ULTRASONIC SENSOR 1
+    delay_led_UP = distanceCm_UP * 2;
+
+    // * ULTRASONIC SENSOR 1 *
+    // Prints the distance in the Serial Monitor 
+    // Green LED twinkle means distance
+    // Red LED means a mistake in the data capturing
+    if (distanceCm_UP < 100)
+      {
+      digitalWrite(vib2, HIGH);
+      delay(delay_led_UP * 2);
+      digitalWrite(vib2, LOW);
+      }
+    if (distanceCm_UP < 500)
+      {
+      digitalWrite(LED_ERROR_UP, LOW);
+      //Serial.print("SENSOR 1 --- Distance (cm): ");
+      //Serial.println(distanceCm_UP);
+      digitalWrite(LED_UP, HIGH);
+      delay(delay_led_UP);
+      digitalWrite(LED_UP, LOW);
+      delay(delay_led_UP); 
+      }
+    else
+      {
+      digitalWrite(vib2, LOW);
+      digitalWrite(LED_ERROR_UP, HIGH);
+      //Serial.print("SENSOR 1 --- Error ---> "); 
+      //Serial.print("Distance (cm): ");
+      //Serial.println(distanceCm_UP);
+      }
+  }
+}
+
+void Sensor2code(void * pvParameters)
+{
+  for(;;)
+  {
+    // * ULTRASONIC SENSOR 2 *
+    // Clears the trigPin
+    digitalWrite(trigPin_DOWN, LOW);
+    delayMicroseconds(2);
+    // Sets the trigPin on HIGH state for 10 micro seconds
+    digitalWrite(trigPin_DOWN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin_DOWN, LOW);
+
+    // Reads the echoPin, returns the sound wave travel time in microseconds
+    duration_DOWN = pulseIn(echoPin_DOWN, HIGH); //ULTRASONIC SENSOR 2
+  
+    // Calculate the distance 
+    distanceCm_DOWN = duration_DOWN * SOUND_SPEED/2; //ULTRASONIC SENSOR 2
+    delay_led_DOWN = distanceCm_DOWN * 2;
+    
+    // * ULTRASONIC SENSOR 2 *
+    // Prints the distance in the Serial Monitor 
+    // Green LED twinkle means distance
+    // Red LED means a mistake in the data capturing
+    if (distanceCm_DOWN < 100)
+      {
+      tone(BUZZER, NOTE_GS5);
+      digitalWrite(vib1, HIGH);
+      delay(delay_led_DOWN * 2);
+      digitalWrite(vib1, LOW);
+      noTone(BUZZER);
+      }
+    if (distanceCm_DOWN < 500)
+      {
+      digitalWrite(LED_ERROR_DOWN, LOW); //ERROR LED OFF
+      //Serial.print("SENSOR 2 --- Distance (cm): ");
+      //Serial.println(distanceCm_DOWN);
+      digitalWrite(LED_DOWN, HIGH);
+      delay(delay_led_DOWN);
+      digitalWrite(LED_DOWN, LOW);
+      delay(delay_led_DOWN); 
+      }
+    else
+      {
+      digitalWrite(vib1, LOW);
+      digitalWrite(LED_ERROR_DOWN, HIGH);
+      //Serial.print("SENSOR 2 --- Error ---> "); 
+      //Serial.print("Distance (cm): ");
+      //Serial.println(distanceCm_DOWN);
+      }
+  }
+}
+
 
 void loop() 
   {
-
+    if (ESP_BT.available()) 
+    {
+    incoming = ESP_BT.read(); //Read what we receive
+    Serial.println(incoming);
+    }
   }
 
     
