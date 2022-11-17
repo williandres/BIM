42f843f4298e6a3396b8b325b3e6c962ac774683
Willian Cárdenas <sca.willian@gmail.com>
2022-11-17 02:48:25 -0500

    BT interpreter

--- Esp-32/bt/bt_test/bt_test.ino
+++ Esp-32/bt/bt_test/bt_test.ino
@@ -1,24 +1,74 @@
 #include "BluetoothSerial.h"
-//BluetoothSerial ESP_BT;
+BluetoothSerial ESP_BT;
 char incoming; 
-char bytes[] =  {'2', '4', '8','3', '6'};
+char bytes[0];
+int i;
 
 void setup() {
   Serial.begin(115200); // Starts the serial communication
-  //ESP_BT.begin("BIM");
+  ESP_BT.begin("BIM");
   
 }
 
 void loop() 
   {
-  for(int i = 0; i < 5; i++)
-{
-  Serial.print(bytes[i]);
-}
-    /*if (ESP_BT.available()) 
+    if (ESP_BT.available()) 
     {
     incoming = ESP_BT.read(); //Read what we receive
-    Serial.println(incoming);
+    
+      if (incoming == 'a')//CREATE A EMPTY LIST
+      {
+        i = 0;
+        bytes[i] = {};
+      }
+      if (incoming == 'z') //EVALUATE
+      {
+        for (byte x = 0; x < i; x = x + 1) 
+          {
+          Serial.println(bytes[x]);
+          }
+        option();
 
-    }*/
+      }
+      else // ADD DATA TO THE THE LIST
+      {
+        bytes[i] = incoming;
+        i = i + 1;
+      }
+    }
   }
+
+
+void option()
+{
+  if (bytes[1] == 'q')
+    {
+       Serial.println("Sensor 1 : ON");
+    }
+  if (bytes[1] == 'w')
+    {
+      Serial.println("Sensor 1 : OFF");
+    }
+  if (bytes[1] == 'e')
+    {
+      Serial.println("Sensor 2 : ON");
+    }
+  if (bytes[1] == 'r')
+    {
+      Serial.println("Sensor 2 : OFF");
+    }
+  if (bytes[1] == 'm')
+    {
+      Serial.println("Assigned distance in Sensor 1");
+    }
+  if (bytes[1] == 'n')
+    {
+      Serial.println("Assigned distance in Sensor 2");
+    }
+
+
+
+
+}
+
+ 
